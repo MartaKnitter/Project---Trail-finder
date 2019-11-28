f0150d054cd6e6e2c9c7b623c5221be2c4cf97f5
Marta Knitter <knittermarta@gmail.com>
2019-11-28 10:54:18 +0100

    Updates

---
+++ precache-manifest.9dd4c01b9dd63208ec214d93b1942759.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324a475f2e7f317dc17eac7d4f4f147e",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "04c019ff5e9e0458d783",
+    "url": "/page/static/css/main.6a889fac.chunk.css"
+  },
+  {
+    "revision": "3eccda12b7336002b0ec",
+    "url": "/page/static/js/2.ace17ea4.chunk.js"
+  },
+  {
+    "revision": "04c019ff5e9e0458d783",
+    "url": "/page/static/js/main.d28b828e.chunk.js"
+  },
+  {
+    "revision": "a073c9f127104f8739a0",
+    "url": "/page/static/js/runtime-main.12fe957c.js"
+  },
+  {
+    "revision": "01ec95d6512571340fc1a3a14cca7a29",
+    "url": "/page/static/media/green-mountain-2132180.01ec95d6.jpg"
+  },
+  {
+    "revision": "078519918a67ea66476d95e93255701f",
+    "url": "/page/static/media/person-on-a-bridge-near-a-lake-747964.07851991.jpg"
+  }
+]);
