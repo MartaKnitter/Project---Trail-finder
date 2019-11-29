12a8f0a7ddd653590801466ea9b2a18533a3e53c
Marta Knitter <knittermarta@gmail.com>
2019-11-29 11:06:03 +0100

    projekt

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -4,69 +4,17 @@ import React, {Component} from "react";
 import Trail from "../Trail/index";
 import './data.css';
 
-
+//funkcyjny
 class Data extends Component {
-    state = {
-        trails: [],
-        result: '',
-        filteredT: []
-    };
-
-    componentDidMount() {
-        this.fetchTrails();
-        this.filterTrail(this.state.trails)
-    }
-
-    fetchTrails = () => {
-        const URL = 'https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&maxResults=100&key=200643499-44644bc81e56ad05e301faa96c26db54';
-        fetch(URL)
-            .then(response => response.json())
-            .then(data => this.setState({ trails: data.trails }))
-            .catch(function(error) {
-                console.log(error)
-            });
-    };
-
-    handleSelectChange = (event) => {
-        this.setState({
-            result: event.target.value
-        }, () => {
-            this.filterTrail(this.state.trails);
-        });
-
-    };
-
-    filterTrail = items => {
-        const wynik = items.filter( el => {
-            if(el.difficulty === this.state.result ) {
-                return el
-            }
-        });
-
-        this.setState({
-            filteredT: wynik
-        })
-    };
-
     render() {
-        const { trails, result, filteredT} = this.state;
-
-        console.log(this.state.filteredT);
-
         return (
-            <>
-                <select name="difficulty" onChange={this.handleSelectChange} className="select-difficulty-button">
-                    <option selected >Wybierz szlak</option>
-                    <option value="blue">Blue</option>
-                    <option value="blueBlack">Blueblack</option>
-                    <option value="black">Black</option>
-                    <option value="greenBlue">Greenblue</option>
-                </select>
-                <div className="flex flex-wrap">
-                {filteredT.map( item => <Trail key={item.id} trail={item} /> )}
-                </div>
-            </>
-
+            <select name="difficulty" onChange={this.props.handleSelectChange} className="select-difficulty-button">
+                <option selected>Wybierz szlak</option>
+                <option value="blue">Blue</option>
+                <option value="blueBlack">Blueblack</option>
+                <option value="black">Black</option>
+                <option value="greenBlue">Greenblue</option>
+            </select>
         )
     }
 }
